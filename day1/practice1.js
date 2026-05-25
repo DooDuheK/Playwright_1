71c7734f13218279c86314689a9727dc6875db2e
DooDuheK <dhinastephen@gmail.com>
2026-05-25 11:47:34 +0530

    Add files via upload

---
+++ day1/practice1.js
@@ -0,0 +1,21 @@
+console.log(12)
+console.log("dhinakar")
+console.log(true)
+
+var empId = 111
+var empName = "dhinakar"
+var empMidterm =false
+var empPark = null
+var empObj = []
+
+console.log(empId)
+console.log(empName)
+console.log(empMidterm)
+console.log(empPark)
+console.log(empObj)
+
+console.log(typeof empId)
+console.log(typeof empName)
+console.log(typeof empMidterm)
+console.log(typeof empPark)
+
